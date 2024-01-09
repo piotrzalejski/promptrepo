f8865638e8895f769c3382e05b629fd7fc0d473e
piotrzalejski <piotrzalejski@outlook.com>
2024-01-09 13:38:47 -0600

    wip feed and promptcards

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,4 +1,70 @@
+'use client';
+import { useState, useEffect } from 'react';
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (data[0].length > 0) {
+      // console.log('postdata: ', data[0]);
+      setLoading(false);
+    }
+  }, [data]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const postData = data[0];
+
+  return (
+    <div className='mt-16 prompt_layout'>
+      {postData &&
+        postData.length > 0 &&
+        postData.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        ))}
+    </div>
+  );
+};
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState('');
+
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/prompt');
+      const data = await res.json();
+
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='search for a tag or username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input'
+        />
+      </form>
+
+      <PromptCardList data={[posts]} handleTagClick={() => {}} />
+    </section>
+  );
 };
 export default Feed;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,4 +1,80 @@
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+'use client';
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import { usePathname, useRouter } from 'next/navigation';
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+
+  const [copied, setCopied] = useState('');
+  const [cardPost, setCardPost] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (post) {
+      setCardPost(post);
+      setLoading(false);
+      //console.log('The card post is: ', post);
+    } else {
+      console.log('No information to display');
+    }
+  }, []);
+
+  const handleCopy = () => {
+    setCopied(cardPost.prompt);
+    navigator.clipboard.writeText(cardPost.prompt);
+    setTimeout(() => setCopied(''), 1000);
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
+        <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
+          <Image
+            src={cardPost.creator.image}
+            width={40}
+            height={40}
+            alt='user image'
+            className='rounded-full object-contain'
+          />
+          <div className='flex flex-col'>
+            <h3 className='font-satoshi font-semibold text-hsl0065'>
+              {post?.creator.username}
+            </h3>
+            <p className='text-sm font-inter text-hsl0065/40'>
+              {post?.creator.email}
+            </p>
+          </div>
+        </div>
+
+        <div className='copy_btn' onClick={handleCopy}>
+          <Image
+            src={
+              copied === post?.prompt
+                ? '/assets/icons/tick.svg'
+                : '/assets/icons/copy.svg'
+            }
+            width={20}
+            height={20}
+            alt='copy'
+          />
+        </div>
+      </div>
+      <pre className='overflow-auto whitespace-pre-line my-4 font-satoshi text-sm text-hsl0065/80'>
+        {post?.prompt}
+      </pre>
+      <p
+        className='text-hsl0065/40 font-inter text-sm cursor-pointer'
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post?.tag}
+      </p>
+    </div>
+  );
 };
 export default PromptCard;
